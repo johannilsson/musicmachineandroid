7963212da9e571518b6b103f315770923d8b46f5
sunkencity <tux@well.com>
2010-05-01 10:59:42 +0200

    testar att använda unik identifier

--- src/com/markupartist/musicmachine/StatusActivity.java
+++ src/com/markupartist/musicmachine/StatusActivity.java
@@ -2,12 +2,16 @@ package com.markupartist.musicmachine;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class StatusActivity extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        TextView textview = new TextView(this);
+        String uID = android.provider.Settings.Secure.ANDROID_ID;
+        textview.setText("Testar id: " + uID);
+        setContentView(textview);
     }
 }
