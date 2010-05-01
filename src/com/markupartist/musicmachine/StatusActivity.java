09ee9d465a97aa6caa846df7e5dc744551dbadaf
Joakim Bodin <joakim.bodin@gmail.com>
2010-05-01 11:48:50 +0200

    Add mock return from spotify gateway, show example in status activity

--- src/com/markupartist/musicmachine/StatusActivity.java
+++ src/com/markupartist/musicmachine/StatusActivity.java
@@ -3,9 +3,13 @@ package com.markupartist.musicmachine;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import com.markupartist.musicmachine.gateway.SpotifyGateway;
+
+import java.util.List;
 
 public class StatusActivity extends Activity implements OnClickListener {
     /** Called when the activity is first created. */
@@ -16,6 +20,11 @@ public class StatusActivity extends Activity implements OnClickListener {
 
         Button searchButton = (Button) findViewById(R.id.search_button);
         searchButton.setOnClickListener(this);
+
+        SpotifyGateway gateway = new SpotifyGateway();
+        List<SpotifyGateway.Track> searchResult = gateway.searchTrack("Foo");
+
+        Log.d("FOO", searchResult.get(0).toString());
     }
 
     @Override
