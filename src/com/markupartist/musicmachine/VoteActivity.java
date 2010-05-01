a88774376f9d220c6cfcf42e4ba05c5f1b46d5c6
Johan Nilsson <github@markupartist.com>
2010-05-01 15:03:31 +0200

    moved voting to a background thread

--- src/com/markupartist/musicmachine/VoteActivity.java
+++ src/com/markupartist/musicmachine/VoteActivity.java
@@ -1,13 +1,17 @@
 package com.markupartist.musicmachine;
 
 import java.io.IOException;
+import java.util.List;
 
 import com.markupartist.musicmachine.gateway.MusicMachineGateway;
+import com.markupartist.musicmachine.gateway.SpotifyGateway;
+import com.markupartist.musicmachine.gateway.SpotifyGatewayTrack;
 import com.markupartist.musicmachine.gateway.MusicMachineGateway.UserHasAlreadyVotedException;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
@@ -46,6 +50,10 @@ public class VoteActivity extends Activity implements OnClickListener {
         switch (v.getId()) {
         case R.id.do_vote_button:
 
+            VoteTask voteTask = new VoteTask();
+            voteTask.execute("");
+
+            /*
         	SharedPreferences sharedPreferences = PreferenceManager
             .getDefaultSharedPreferences(this);
 
@@ -64,8 +72,55 @@ public class VoteActivity extends Activity implements OnClickListener {
 				}
         	
             Toast.makeText(this, "VOTE user id: " + userId, Toast.LENGTH_SHORT).show();
+            */
             break;
         }
         
+    }
+
+    /**
+     * Background job for voting...
+     */
+    private class VoteTask extends AsyncTask<String, Void, Boolean> {
+        private boolean mWasSuccess = true;
+
+        @Override
+        public void onPreExecute() {
+            //showProgress();
+        }
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            SharedPreferences sharedPreferences = PreferenceManager
+                    .getDefaultSharedPreferences(VoteActivity.this);
+
+            String userId = sharedPreferences.getString("username", "");
+
+            MusicMachineGateway mmGateway = new MusicMachineGateway(sharedPreferences.getString("server_url", ""));
+
+            try {
+                mmGateway.vote(mExtras.getString("mm.uri"), userId);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return false;
+            } catch (UserHasAlreadyVotedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                return false;
+            }
+
+            return true;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            if (result == true) {
+                Toast.makeText(VoteActivity.this, "Network problem...", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(VoteActivity.this, "Failed to vote...", Toast.LENGTH_SHORT).show();
+            }
+            
+        }
     }
 }
