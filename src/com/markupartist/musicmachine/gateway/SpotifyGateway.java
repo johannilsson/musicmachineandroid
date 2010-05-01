69268ec7f0b8ce0b66a36a0612af452e914b8117
Joakim Bodin <joakim.bodin@gmail.com>
2010-05-01 14:58:13 +0200

    Use sax parser for spotify gateway

--- src/com/markupartist/musicmachine/gateway/SpotifyGateway.java
+++ src/com/markupartist/musicmachine/gateway/SpotifyGateway.java
@@ -1,6 +1,14 @@
 package com.markupartist.musicmachine.gateway;
 
-import java.util.ArrayList;
+import android.util.Log;
+import com.markupartist.musicmachine.utils.HttpManager;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.xml.sax.InputSource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
 import java.util.List;
 
 /**
@@ -11,92 +19,31 @@ import java.util.List;
  * To change this template use File | Settings | File Templates.
  */
 public class SpotifyGateway {
+    private static String TAG = "SpotifyGateway";
+
     public List<SpotifyGatewayTrack> searchTrack(String searchTerm)
     {
-        List<SpotifyGatewayTrack> searchResult = new ArrayList<SpotifyGatewayTrack>();
+        List<SpotifyGatewayTrack> searchResult;
 
-        SpotifyGatewayTrack mockedTrack = new SpotifyGatewayTrack("Kaizers Orchestra", "Prosessen", "Våre Demoner", "spotify:title:5S9edTU6BpUmp0enwiYoZ6");
-        searchResult.add(mockedTrack);
-
-        return searchResult;
-
-        /*
-
-        HttpHost searchHttpHost = new HttpHost("ws.spotify.com", 80);
-
-        URI searchUrl = null;
-        try {
-            searchUrl = new URI("http", "", "ws.spotify.com", 80, "/search/1/title.xml", "q=" + searchTerm, "");
-        } catch (URISyntaxException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-
-        HttpGet searchHttpGet = new HttpGet(searchUrl);
+        HttpGet searchHttpGet = new HttpGet("http://ws.spotify.com/search/1/track.xml?q=" + URLEncoder.encode(searchTerm));
 
         HttpResponse response = null;
         try {
             response = HttpManager.execute(searchHttpGet);
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            Log.e(TAG, "HttpManager.execute", e);
         }
 
         InputStream responseContentStream = null;
         try {
             responseContentStream = response.getEntity().getContent();
         } catch (IOException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            Log.e(TAG, "getContent", e);
         }
 
-        SAXReader reader = new SAXReader(); // dom4j SAXReader
-        Document document = null; // dom4j Document
-        try {
-            document = reader.read(responseContentStream);
-        } catch (DocumentException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-
-        */
-
-        /*
-          <track xmlns="http://www.spotify.com/ns/music/1" href="spotify:title:5S9edTU6BpUmp0enwiYoZ6">
-            <name>Prosessen</name>
-            <artist href="spotify:artist:1s1DnVoBDfp3jxjjew8cBR">
-              <name>Kaizers Orchestra</name>
-            </artist>
-            <id type="isrc">NOHDL0901100</id>
-            <album href="spotify:album:6CSTOAMHphZGon9leEyN9c">
-              <name>Våre Demoner</name>
-              <released>2009</released>
-              <availability>
-                <territories>AD AE AF AG AI AL AM AN AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BM BN BO BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IN IO IQ IR IS IT JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR ST SV SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW</territories>
-              </availability>
-            </album>
-            <track-number>10</track-number>
-            <length>250.280000</length>
-            <popularity>0.73049</popularity>
-          </track>
-        */
-
-        /*
-
-        List<Node> trackNodes = document.selectNodes("//tracks/track");
-        for (Node trackNode : trackNodes)
-        {
-            SpotifyGateway.Track currenTrack = new SpotifyGateway.Track();
-
-            Node artistNode = trackNode.selectSingleNode("artist/name");
-            currenTrack.setArtist(artistNode.getText());
-
-            Node albumNode = trackNode.selectSingleNode("album/name");
-            currenTrack.setAlbum(albumNode.getText());
-
-            Node trackNameNode = trackNode.selectSingleNode("name");
-            currenTrack.setTitle(trackNameNode.getText());
-
-            currenTrack.setUri(trackNode.valueOf("@href"));
-        }
+        SpotifyGatewayTrackParser parser = new SpotifyGatewayTrackParser();
+        searchResult = parser.parseTracks(new InputSource(responseContentStream));
 
         return searchResult;
-        */
     }
 }
---
+++ src/com/markupartist/musicmachine/gateway/SpotifyGatewayTrackParser.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright (C) 2009 Johan Nilsson <http://markupartist.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.markupartist.musicmachine.gateway;
+
+import android.util.Log;
+import org.xml.sax.Attributes;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class SpotifyGatewayTrackParser extends DefaultHandler {
+    private static final String TAG = "StopFinder";
+    private StringBuilder mTextBuffer = null;
+    boolean mIsBuffering = false;
+    private ArrayList<SpotifyGatewayTrack> mTracks = new ArrayList<SpotifyGatewayTrack>();
+    private SpotifyGatewayTrack mCurrentTrack;
+    private boolean isInArtist = false;
+    private boolean isInAlbum = false;
+    private boolean isInTrack = false;
+
+    public ArrayList<SpotifyGatewayTrack> parseTracks(InputSource input) {
+        mTracks.clear();
+        try {
+            SAXParserFactory spf = SAXParserFactory.newInstance();
+            SAXParser sp = spf.newSAXParser();
+            XMLReader xr = sp.getXMLReader();
+            xr.setContentHandler(this);
+            input.setEncoding("UTF-8");
+            xr.parse(input);
+        } catch (IOException e) {
+            Log.e(TAG, e.toString());
+        } catch (SAXException e) {
+            Log.e(TAG, e.toString());
+        } catch (ParserConfigurationException e) {
+            Log.e(TAG, e.toString());
+        }
+
+        return mTracks;
+    }
+
+    /*
+      <track xmlns="http://www.spotify.com/ns/music/1" href="spotify:title:5S9edTU6BpUmp0enwiYoZ6">
+        <name>Prosessen</name>
+        <artist href="spotify:artist:1s1DnVoBDfp3jxjjew8cBR">
+          <name>Kaizers Orchestra</name>
+        </artist>
+        <id type="isrc">NOHDL0901100</id>
+        <album href="spotify:album:6CSTOAMHphZGon9leEyN9c">
+          <name>Våre Demoner</name>
+          <released>2009</released>
+          <availability>
+            <territories>AD AE AF AG AI AL AM AN AO AQ AR AS AT AU AW AX AZ BA BB BD BE BF BG BH BI BJ BM BN BO BR BS BT BV BW BY BZ CA CC CD CF CG CH CI CK CL CM CN CO CR CU CV CX CY CZ DE DJ DK DM DO DZ EC EE EG EH ER ES ET FI FJ FK FM FO FR GA GB GD GE GF GG GH GI GL GM GN GP GQ GR GS GT GU GW GY HK HM HN HR HT HU ID IE IL IN IO IQ IR IS IT JM JO JP KE KG KH KI KM KN KP KR KW KY KZ LA LB LC LI LK LR LS LT LU LV LY MA MC MD ME MG MH MK ML MM MN MO MP MQ MR MS MT MU MV MW MX MY MZ NA NC NE NF NG NI NL NO NP NR NU NZ OM PA PE PF PG PH PK PL PM PN PR PS PT PW PY QA RE RO RS RU RW SA SB SC SD SE SG SH SI SJ SK SL SM SN SO SR ST SV SY SZ TC TD TF TG TH TJ TK TL TM TN TO TR TT TV TW TZ UA UG UM US UY UZ VA VC VE VG VI VN VU WF WS YE YT ZA ZM ZW</territories>
+          </availability>
+        </album>
+        <track-number>10</track-number>
+        <length>250.280000</length>
+        <popularity>0.73049</popularity>
+      </track>
+    */
+    public void startElement(String uri, String name, String qName, Attributes atts) {
+        if (name.equals("track")) {
+            isInTrack = true;
+            mCurrentTrack = new SpotifyGatewayTrack();
+            mCurrentTrack.setUri(atts.getValue("href"));
+        } else if (name.equals("artist")) {
+            isInArtist = true;
+        } else if (name.equals("album")) {
+            isInAlbum = true;
+        } else if (name.equals("name") && isInTrack) {
+            startBuffer();
+        } else if (name.equals("name") && isInArtist) {
+            startBuffer();
+        } else if (name.equals("name") && isInAlbum) {
+            startBuffer();
+        }
+    }
+
+    public void characters(char ch[], int start, int length) {
+        if (mIsBuffering) {
+            mTextBuffer.append(ch, start, length);
+        }
+    }
+
+    public void endElement(String uri, String name, String qName)
+                throws SAXException {
+        if (name.trim().equals("track")) {
+            isInTrack = false;
+            mTracks.add(mCurrentTrack);
+        } else if (name.equals("artist")) {
+            isInArtist = false;
+        } else if (name.equals("album")) {
+            isInAlbum = false;
+        } else if (name.trim().equals("name") && isInTrack) {
+            endBuffer();
+            mCurrentTrack.setTitle(mTextBuffer.toString());
+        } else if (name.trim().equals("name") && isInArtist) {
+            endBuffer();
+            mCurrentTrack.setArtist(mTextBuffer.toString());
+        } else if (name.trim().equals("name") && isInAlbum) {
+            endBuffer();
+            mCurrentTrack.setAlbum(mTextBuffer.toString());
+        }
+    }
+
+    private void startBuffer() {
+        mTextBuffer = new StringBuilder();
+        mIsBuffering = true;
+    }
+
+    private void endBuffer() {
+        mIsBuffering = false;
+    }
+}
