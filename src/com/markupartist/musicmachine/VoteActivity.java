48dad27292860ad3082ddc7e65f6ff5471de5dc9
Joakim Bodin <joakim.bodin@gmail.com>
2010-05-01 17:38:16 +0200

    Make preview button play song in spotify

--- src/com/markupartist/musicmachine/VoteActivity.java
+++ src/com/markupartist/musicmachine/VoteActivity.java
@@ -1,8 +1,13 @@
 package com.markupartist.musicmachine;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -22,15 +27,16 @@ import java.security.NoSuchAlgorithmException;
 
 public class VoteActivity extends Activity implements OnClickListener {
     private Bundle mExtras;
+    private String spotifyUri;
 
 	/** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vote);
-
         
         mExtras = getIntent().getExtras();
+        spotifyUri = mExtras.getString("mm.uri");
      
         TextView artistView = (TextView) findViewById(R.id.vote_artist);
         artistView.setText(mExtras.getString("mm.artist"));
@@ -43,39 +49,47 @@ public class VoteActivity extends Activity implements OnClickListener {
         
         Button searchButton = (Button) findViewById(R.id.do_vote_button);
         searchButton.setOnClickListener(this);
+
+        Button previewButton = (Button) findViewById(R.id.preview_button);
+        previewButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-        case R.id.do_vote_button:
+        case R.id.preview_button:
+            try {
+                Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(spotifyUri));
+                startActivity(i);
+            } catch (ActivityNotFoundException e) {
+                showDialog(0);
+            }
+            break;
 
+        case R.id.do_vote_button:
             VoteTask voteTask = new VoteTask();
             voteTask.execute("");
-
-            /*
-        	SharedPreferences sharedPreferences = PreferenceManager
-            .getDefaultSharedPreferences(this);
-
-        	String userId = sharedPreferences.getString("username", "");
-
-        	MusicMachineGateway mmGateway = new MusicMachineGateway(sharedPreferences.getString("server_url", ""));
-
-        	try {
-					mmGateway.vote(mExtras.getString("mm.uri"), userId);
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (UserHasAlreadyVotedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-        	
-            Toast.makeText(this, "VOTE user id: " + userId, Toast.LENGTH_SHORT).show();
-            */
             break;
         }
         
+    }
+
+    @Override
+    public Dialog onCreateDialog(int id)
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        switch(id)
+        {
+            case 0:
+            {
+                builder.setTitle("Spotify not found");
+                builder.setMessage("Spotify is needed to preview a song, please download it from the Android Market");
+                builder.setPositiveButton("OK", null);
+            }
+        }
+
+        return builder.create();
     }
 
     /**
