692f6112564c207d80fbacd72b88bf8215e675f1
Joakim Bodin <joakim.bodin@gmail.com>
2010-05-01 10:23:35 +0200

    Add intellij project, workable XML lib and gateway package

---
+++ src/com/markupartist/musicmachine/gateway/SpotifyGateway.java
@@ -0,0 +1,11 @@
+package com.markupartist.musicmachine.gateway;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: joakimb
+ * Date: May 1, 2010
+ * Time: 10:20:43 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class SpotifyGateway {
+}
