ef99e6429fe78a94c6fb14411f815b4b0c997d89
Johan Nilsson <github@markupartist.com>
2010-05-04 20:45:08 +0200

    added autocomplete for search results and a no results view

--- src/com/markupartist/musicmachine/SearchActivity.java
+++ src/com/markupartist/musicmachine/SearchActivity.java
@@ -4,6 +4,8 @@ import android.app.ListActivity;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -31,6 +33,8 @@ public class SearchActivity extends ListActivity implements OnClickListener, OnE
 
         mSearchView = (AutoCompleteTextView) findViewById(R.id.search_text);
         mSearchView.setOnEditorActionListener(this);
+        mSearchView.addTextChangedListener(new SearchTextWatcher());
+
         ImageButton searchButton = (ImageButton) findViewById(R.id.btn_search);
         searchButton.setOnClickListener(this);
     }
@@ -39,7 +43,7 @@ public class SearchActivity extends ListActivity implements OnClickListener, OnE
     public void onClick(View v) {
         switch (v.getId()) {
         case R.id.btn_search:
-            doSearch();
+            doSearch(mSearchView.getText().toString());
             break;
         }
     }
@@ -49,7 +53,7 @@ public class SearchActivity extends ListActivity implements OnClickListener, OnE
         if(v.length() > 0) {
             boolean isEnterKey = (null != event && event.getKeyCode() == KeyEvent.KEYCODE_ENTER);
             if(actionId == EditorInfo.IME_ACTION_SEARCH || true == isEnterKey) {
-                doSearch();
+                doSearch(mSearchView.getText().toString());
                 return true;
             }
         }
@@ -58,7 +62,9 @@ public class SearchActivity extends ListActivity implements OnClickListener, OnE
 
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
+        @SuppressWarnings("unchecked")
         Map<String, Object> item = (Map<String, Object>) ((SimpleAdapter)getListAdapter()).getItem(position);
+
         SpotifyGatewayTrack track = (SpotifyGatewayTrack) item.get("track");
         Log.d(TAG, "track: " + track.getArtist());
 
@@ -75,16 +81,18 @@ public class SearchActivity extends ListActivity implements OnClickListener, OnE
         super.onListItemClick(l, v, position, id);
     }
 
-    private void doSearch() {
+    private void doSearch(String searchQuery) {
         ImageView startHereView = (ImageView) findViewById(R.id.start_here);
         startHereView.setVisibility(View.GONE);
 
+        LinearLayout noResultView = (LinearLayout) findViewById(R.id.search_no_result);
+        noResultView.setVisibility(View.GONE);
+
         LinearLayout progressBar = (LinearLayout) findViewById(R.id.search_progress);
         progressBar.setVisibility(View.VISIBLE);
-        
-        String searchText = mSearchView.getText().toString();
+
         GetTracks trackTask = new GetTracks();
-        trackTask.execute(searchText);
+        trackTask.execute(searchQuery);
     }
 
     private void onSearchResult(List<SpotifyGatewayTrack> result) {
@@ -93,7 +101,14 @@ public class SearchActivity extends ListActivity implements OnClickListener, OnE
 
         setListAdapter(createResultAdapter(result));        
     }
-    
+
+    private void onNoSearchResult() {
+        LinearLayout progressBar = (LinearLayout) findViewById(R.id.search_progress);
+        progressBar.setVisibility(View.GONE);
+        LinearLayout noResultView = (LinearLayout) findViewById(R.id.search_no_result);
+        noResultView.setVisibility(View.VISIBLE);
+    }
+
     private SimpleAdapter createResultAdapter(List<SpotifyGatewayTrack> tracks) {
         ArrayList<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
 
@@ -160,8 +175,33 @@ public class SearchActivity extends ListActivity implements OnClickListener, OnE
             } else if (!mWasSuccess) {
                 Toast.makeText(SearchActivity.this, "Network problem...", Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(SearchActivity.this, "No result...", Toast.LENGTH_SHORT).show();
+                onNoSearchResult();
             }
         }
+    }
+
+    private class SearchTextWatcher implements TextWatcher {
+        private boolean doSearch = false;
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (doSearch) {
+                doSearch(s.toString());
+            }
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count,
+                int after) {
+            if (start > 2) {
+                doSearch = true;
+            }
+        }
+
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before,
+                int count) {
+            // Needed by interface.
+        }
+        
     }
 }
